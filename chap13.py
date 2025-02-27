c8ed9ccaf07f8637236eb8031d76a13138c8e5a1
Hoa0304 <tranthicamhoa2k4@gmail.com>
2025-02-27 08:35:40 +0700

    #1: Chapter 13 practice 3

--- chap13.py
+++ chap13.py
@@ -76,3 +76,28 @@ def create_histogram(file_name):
 # Tạo histogram từ file văn bản
 hist = create_histogram("words.txt")
 print(choose_from_hist(hist))  # Chọn ngẫu nhiên một từ từ histogram
+
+def process_file(filename):
+    hist = dict()
+    fp = open(filename)
+    for line in fp:
+        process_line(line, hist)
+    return hist
+
+def process_line(line, hist):
+    line = line.replace('-', ' ')
+    for word in line.split():
+        word = word.strip(string.punctuation + string.whitespace)
+        word = word.lower()
+        hist[word] = hist.get(word, 0) + 1
+
+hist = process_file('words.txt')
+
+def total_words(hist):
+    return sum(hist.values())
+
+def different_words(hist):
+    return len(hist)
+
+print('Total number of words:', total_words(hist))
+print('Number of different words:', different_words(hist))
